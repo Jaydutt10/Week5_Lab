c5f5ab5f8aa4996a5793e7ee9df22dc2dac6e722
jaydu <jaydu@JDs-OMEN>
2023-02-12 20:51:08 -0700

    Login page website

---
+++ src/java/models/User.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Jaydutt
+ */
+public class User {
+    private String username;
+    private String password;
+    
+    public String getUsername() {
+        return this.username;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+}
---
+++ web/WEB-INF/home.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : home
+    Created on : 12-Feb-2023, 7:07:03 PM
+    Author     : jaydutt
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Home page</title>
+    </head>
+    <body>
+        <h1>Home Page</h1>
+        <h3>Hello abe </h3>
+        <a href="login?logout">Log out</a>
+    </body>
+</html>
